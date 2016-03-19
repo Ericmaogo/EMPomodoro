584341f8396a118a0227468368149f8c19061cd0
maoyuxi <maoyuxi20072683@gmail.com>
2016-03-19 18:26:22 +0800

    Initial Commit

---
+++ EMPomodoro/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EMPomodoro
+//
+//  Created by Ericmao on 16/3/19.
+//  Copyright © 2016年 zhengya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ EMPomodoro/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  EMPomodoro
+//
+//  Created by Ericmao on 16/3/19.
+//  Copyright © 2016年 zhengya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
